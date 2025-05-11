3e10d86c59b073d34f45be15367dc8b97e96d60e
deadofgreaf <deadofgreaf@gmail.com>
2025-05-11 12:20:53 +0300

    Add Graphana n Prometheus

--- server/src/EuroMotors.Api/Program.cs
+++ server/src/EuroMotors.Api/Program.cs
@@ -8,6 +8,7 @@ using EuroMotors.Infrastructure;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Prometheus;
 using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -21,12 +22,13 @@ builder.Services
     .AddPresentation()
     .AddInfrastructure(builder.Configuration);
 
+builder.Services.AddHealthChecks()
+    .ForwardToPrometheus();
+
 if (!builder.Environment.IsEnvironment("Testing"))
 {
     X509Certificate2 cert = X509CertificateLoader.LoadPkcs12FromFile(
-        builder.Environment.IsDevelopment()
-            ? "./ssl/euromotors_tech.pfx"
-            : "/app/ssl/euromotors_tech.pfx",
+        "./ssl/euromotors_tech.pfx",
         "656bb9bb-16d7-496c-90a7-5dc7ae559128"
     );
 
@@ -53,13 +55,26 @@ if (app.Environment.IsDevelopment() || builder.Environment.IsEnvironment("Testin
     app.SeedData(passwordHasher);
 }
 
-
+app.UseHttpMetrics();
 
 app.UseRequestContextLogging();
 
 app.UseSerilogRequestLogging();
 
 app.UseExceptionHandler();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path.StartsWithSegments("/metrics"))
+    {
+        context.Response.Headers.Remove("Location");
+        await next();
+    }
+    else
+    {
+        await next();
+    }
+});
 
 app.UseHttpsRedirection();
 
@@ -77,6 +92,9 @@ if (!builder.Environment.IsEnvironment("Testing"))
     {
         ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
     });
+    
+    app.MapMetrics().WithMetadata(new EuroMotors.Api.HttpsRedirectionDisableMetadata());
+    
     app.UseSpaFallback();
 }
 
@@ -85,4 +103,8 @@ await app.RunAsync();
 namespace EuroMotors.Api
 {
     public partial class Program;
+    
+    public class HttpsRedirectionDisableMetadata
+    {
+    }
 }
